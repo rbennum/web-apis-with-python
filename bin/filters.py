a2c335fde129722f5a2f5f370ecb0cc4e76ca67a
Bening Ranum <8705838+rbennum@users.noreply.github.com>
2025-03-07 01:03:06 +0700

    feat: add image filtering

--- bin/filters.py
+++ bin/filters.py
@@ -4,12 +4,17 @@ import io
 
 def apply_filter(file: object, filter: str) -> object:
     """
-    TODO:
     1. Accept the image as file object, and the filter type as string
     2. Open the as an PIL Image object
     3. Apply the filter
     4. Convert the PIL Image object to file object
     5. Return the file object
     """
+    image = Image.open(file)
+    image = image.filter(eval(f"ImageFilter.{filter.upper()}"))
 
-    pass
+    file = io.BytesIO()
+    image.save(file, "JPEG")
+    file.seek(0)
+
+    return file
